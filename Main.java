c4b6e808d7e6e38462ed791ee53b222439fc2d3f
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-02-24 13:15:20 +0300

    HomeWork

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import domen.Student;
 import domen.StudentGroup;
 import domen.StudentIterator;
+import domen.StudentSteam;
 
 import java.util.Collections;
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        List<Student> students = new ArrayList<>();
+        List<Student> students1 = new ArrayList<>();
         Student student1 = new Student("Ivan", 20);
         Student student2 = new Student("Nik", 22);
         Student student3 = new Student("Nikolas", 19);
@@ -16,22 +17,71 @@ public class Main {
         Student student5= new Student("Bro", 18);
         Student student6= new Student("Olga", 18);
         Student student7= new Student("Dima", 18);
-        students.add(student1);
-        students.add(student2);
-        students.add(student3);
-        students.add(student4);
-        students.add(student5);
-        students.add(student6);
-        students.add(student7);
-        StudentGroup group = new StudentGroup(2, students);
-        for (Student student : group) {
+        Student student8= new Student("Tomi", 23);
+        Student student9= new Student("Ryan Gosling", 24);
+        Student student10= new Student("Brother", 25);
+        Student student11 = new Student("Bill", 23);
+        Student student12 = new Student("Richard", 21);
+        Student student13 = new Student("Franklin", 26);
+
+        students1.add(student1);
+        students1.add(student2);
+        students1.add(student3);
+        students1.add(student4);
+        students1.add(student5);
+        students1.add(student6);
+        students1.add(student7);
+
+        StudentGroup studentGroup1 = new StudentGroup(1, students1);
+
+        for (Student student : studentGroup1) {
             System.out.println(student);
         }
         System.out.println("------------");
-        Collections.sort(group.getStudentList());
+        Collections.sort(studentGroup1.getStudentList());
         System.out.println("------------");
-        for (Student student : group) {
+        for (Student student : studentGroup1) {
             System.out.println(student);
         }
+
+        List<Student> students2 = new ArrayList<>(); // Второй список студентов
+        List<Student> students3 = new ArrayList<>(); // Третий список студентов
+
+        // Добавление студентов в разные списки
+        students2.add(student8);
+        students2.add(student9);
+        students2.add(student10);
+        students3.add(student11);
+        students3.add(student12);
+        students3.add(student13);
+
+        // Создание групп для студентов
+        List<StudentGroup> studentGroupList1 = new ArrayList<>();
+        List<StudentGroup> studentGroupList2 = new ArrayList<>();
+        StudentGroup studentGroup2 = new StudentGroup(2, students2);
+        StudentGroup studentGroup3 = new StudentGroup(3, students3);
+
+        // Добавление студентов в эти потоки
+        studentGroupList1.add(studentGroup1);
+        studentGroupList1.add(studentGroup1);
+        studentGroupList2.add(studentGroup2);
+        studentGroupList2.add(studentGroup3);
+
+        StudentSteam studentSteam1 = new StudentSteam(1, studentGroupList1);
+        StudentSteam studentSteam2 = new StudentSteam(2, studentGroupList2);
+        System.out.println("studentSteam1");
+        System.out.println("------------");
+        Collections.sort(studentSteam1.getStudentList());
+        for (StudentGroup studentGroup : studentSteam1) {
+            System.out.println(studentGroup);
+        }
+        System.out.println("------------");
+        Collections.sort(studentSteam2.getStudentList());
+        for (StudentGroup studentGroup : studentSteam2) {
+            System.out.println(studentGroup);
+        }
+
+
+
     }
 }
