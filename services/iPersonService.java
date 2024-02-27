f1d5080cf4799da664ef9716e8717fe1b6b572ff
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-02-28 01:13:42 +0300

    End of 4th Seminar

--- services/iPersonService.java
+++ services/iPersonService.java
@@ -1,2 +1,8 @@
-package services;public interface iPersonService {
+package services;
+
+import java.util.List;
+
+public interface iPersonService<T> {
+    public List<T> getAll();
+    public void create(String name, int age);
 }
