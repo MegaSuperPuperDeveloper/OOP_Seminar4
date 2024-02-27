ff5e500a5dea45cef9ebabbaf3e935facd5df5d0
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-02-28 01:14:31 +0300

    End of 4th Seminar

--- domen/PersonComparator.java
+++ domen/PersonComparator.java
@@ -1,2 +1,12 @@
-package domen;public class PersonComparator {
+package domen;
+
+import java.util.Comparator;
+
+public class PersonComparator<T extends Person> implements Comparator<T> {
+
+    @Override
+    public int compare(T o1, T o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
+
 }
