d7298f09d85b80722ef84a19cefd2afbde07517c
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-02-28 01:14:21 +0300

    End of 4th Seminar

--- domen/Employee.java
+++ domen/Employee.java
@@ -1,6 +1,6 @@
 package domen;
 
-public class Employee extends Person{
+public class Employee extends WorkingPerson{
     private String special;
 
     public Employee(String name, int age, String special) {
