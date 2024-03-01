687f217c8002aa83d35605ed53152afcfe2fe71d
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-03-01 17:34:48 +0300

    HomeWork

--- services/TeacherService.java
+++ services/TeacherService.java
@@ -1,2 +1,56 @@
-package services;public class TeacherService {
+package services;
+
+import domen.Employee;
+import domen.PersonComparator;
+import domen.Teacher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TeacherService implements iPersonService<Teacher> {
+    private int count;
+    private List<Teacher> teachers;
+
+
+    public TeacherService() {
+        this.teachers = new ArrayList<Teacher>();
+    }
+
+    @Override
+    public List<Teacher> getAll() {
+        return teachers;
+    }
+
+    @Override
+    public void create(String name, int age) {
+        teachers.add(new Teacher(name, age, "Мучитель"));
+        this.count++;
+    }
+
+    public void printAll() {
+
+        for (Teacher teacher : teachers) {
+            System.out.println(teacher);
+        }
+    }
+
+    public void sortByFIO() {
+        this.teachers.sort(new PersonComparator<Teacher>());
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(List<Teacher> teachers) {
+        this.teachers = teachers;
+    }
 }
